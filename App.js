463ca8031a1bc17aa121fdb11f39fbe246984ff4
HYEOK_E <jhjeong00@gmail.com>
2021-08-02 02:23:43 +0900

    native 사용법 적응 중 추후 개발 시작

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as AiIcons from 'react-icons/ai';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Ionicons name="menu" style={styles.icon} />
+            <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 40,
+    },
+    icon: {
+        fontSize: 50,
+    },
 });
